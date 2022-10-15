cf8c41289c5c5b085fbfe5a76c571e6eb04db57f
zephyr664 <dbrgamei5@gmail.com>
2022-10-15 06:07:48 -0300

    aula 2

---
+++ AluraOO-cpp/AluraOO-cpp/conta.cpp
@@ -0,0 +1,8 @@
+//
+//  conta.cpp
+//  AluraOO-cpp
+//
+//  Created by Diego Sabino on 15/10/22.
+//
+
+#include "conta.hpp"
---
+++ AluraOO-cpp/AluraOO-cpp/conta.hpp
@@ -0,0 +1,13 @@
+//
+//  conta.hpp
+//  AluraOO-cpp
+//
+//  Created by Diego Sabino on 15/10/22.
+//
+
+#ifndef conta_hpp
+#define conta_hpp
+
+#include <stdio.h>
+
+#endif /* conta_hpp */
